3d0666ca1e6dfa644d334fc818c549b1404db42b
suncloudsmoon <gajjam2003@gmail.com>
2022-03-11 19:24:05 -0800

    Minor fixes

--- drivers/ata.cpp
+++ drivers/ata.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2022, suncloudsmoon and the Simple-Cpp-Kernel contributors.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <drivers/ata.hpp>
 #include <drivers/instr.hpp>
 
@@ -7,48 +26,10 @@
 
 namespace os {
 	namespace drivers {
-		ata::ata(unsigned poll_lim) : ata_init_success(false), polling_limit(poll_lim) {
+		ata::ata(unsigned polling_limit) : ata_init_success(true), poll_lim(polling_limit) {
 			identity_cmd();
 		}
-		ata::~ata() {
-
-		}
-
-		// TODO: fix memory leak resulting from data_packet... maybe add a destructor?
-		data_packet ata::read(int drive_bit, CHS addr, uint16_t num_sectors) {
-			uint16_t num_read = num_sectors * 256;
-			data_packet packet = { new uint16_t[num_read](), num_read };
-
-			instr::outb(ports::sec_count, num_sectors);
-			instr::outb(ports::sec_num, addr.sector);
-			instr::outb(ports::cylin_low, addr.cylinder & 0xFF); // zero out 0000 0000 1111 1111
-			instr::outb(ports::cylin_high, addr.cylinder >> 8);
-			instr::outb(ports::drive_head_select, drive_type::master_drive | (drive_bit << 4) | addr.head);
-
-			instr::outb(ports::command, commands::read_sec);
-
-			for (size_t i = 0; i < packet.size; i++)
-				packet.data[i] = instr::inw(ports::data);
-
-			return packet;
-		}
-		bool ata::write(int drive_bit, CHS addr, data_packet dat) {
-			if (!dat) return false;
-			
-			instr::outb(ports::sec_count, (dat.size * sizeof(uint16_t)) / 512);
-			instr::outb(ports::sec_num, addr.sector);
-			instr::outb(ports::cylin_low, addr.cylinder & 0xFF); // zero out 0000 0000 1111 1111
-			instr::outb(ports::cylin_high, addr.cylinder & 0xFF00);
-			instr::outb(ports::drive_head_select, drive_type::master_drive | (drive_bit << 4) | addr.head);
-
-			// changed from outb to outw
-			instr::outb(ports::command, commands::write_sec);
-
-			for (size_t i = 0; i < dat.size; i++)
-				instr::outw(ports::data, dat.data[i]);
-
-			return true;
-		}
+		ata::~ata() {}
 
 		void ata::identity_cmd() {
 			instr::outb(ports::drive_head_select, drive_type::master_drive);
@@ -60,7 +41,7 @@ namespace os {
 			// Polling for the status
 			unsigned polling_index = 0;
 			uint16_t status;
-			while (polling_index++ < polling_limit) {
+			while (polling_index++ < poll_lim) {
 				status = instr::inb(ports::status);
 				if ((status & 128) == 0) break;
 			}
@@ -82,6 +63,58 @@ namespace os {
 	zl::cout << zl::endl;	
 	zl::cout << "End of Ata Driver Testing" << zl::endl;
 #endif
+		}
+
+		// TODO: fix memory leak resulting from data_packet... maybe add a destructor?
+		data_packet ata::read(int drive_bit, CHS addr, uint16_t num_sectors) {
+			uint16_t num_read = num_sectors * 256;
+			data_packet packet = { new uint16_t[num_read](), num_read };
+
+			instr::outb(ports::sec_count, num_sectors);
+			instr::outb(ports::sec_num, addr.sector);
+			instr::outb(ports::cylin_low, addr.cylinder & 0xFF); // zero out 0000 0000 1111 1111
+			instr::outb(ports::cylin_high, addr.cylinder >> 8);
+			instr::outb(ports::drive_head_select, drive_type::master_drive | (drive_bit << 4) | addr.head);
+
+			instr::outb(ports::command, commands::read_sec);
+
+			// Wait until the device is ready to tranfer data
+			unsigned poll_index = 0;
+			while (poll_index++ < poll_lim) {
+				uint16_t status = instr::inw(ports::status);
+				// (Bit 3 and 7) must be 0 or (Bit 0 or 5) is 1
+				if (((status & 8) == 0 && (status & 128) == 0) || ((status & 1) != 0 || (status & 32) != 0)) break;
+			}
+
+			for (size_t i = 0; i < packet.size; i++)
+				packet.data[i] = instr::inw(ports::data);
+
+			return packet;
+		}
+		bool ata::write(int drive_bit, CHS addr, data_packet dat) {
+			if (!dat) return false;
+			
+			instr::outb(ports::sec_count, (dat.size * sizeof(uint16_t)) / 512);
+			instr::outb(ports::sec_num, addr.sector);
+			instr::outb(ports::cylin_low, addr.cylinder & 0xFF); // zero out 0000 0000 1111 1111
+			instr::outb(ports::cylin_high, addr.cylinder & 0xFF00);
+			instr::outb(ports::drive_head_select, drive_type::master_drive | (drive_bit << 4) | addr.head);
+
+			// changed from outb to outw
+			instr::outb(ports::command, commands::write_sec);
+
+			// Wait until the device is ready to tranfer data
+			unsigned poll_index = 0;
+			while (poll_index++ < poll_lim) {
+				uint16_t status = instr::inw(ports::status);
+				// (Bit 3 and 7) must be 0 or (Bit 0 or 5) is 1
+				if (((status & 8) == 0 && (status & 128) == 0) || ((status & 1) != 0 || (status & 32) != 0)) break;
+			}
+
+			for (size_t i = 0; i < dat.size; i++)
+				instr::outw(ports::data, dat.data[i]);
+
+			return true;
 		}
 	}
 }
--- include/drivers/ata.hpp
+++ include/drivers/ata.hpp
@@ -2,6 +2,25 @@
 #define DRIVERS_ATA_HPP
 
 #include <stdint.h>
+/*
+ * Copyright (c) 2022, suncloudsmoon and the Simple-Cpp-Kernel contributors.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <stddef.h>
 
 namespace os {
@@ -55,7 +74,7 @@ namespace os {
 
 		class ata {
 			public:
-				ata(unsigned poll_lim = 1000);
+				ata(unsigned polling_limit = 1000);
 				~ata();
 				operator bool() { return ata_init_success; }
 				data_packet read(int drive_bit, CHS addr, uint16_t num_sectors);
@@ -63,7 +82,7 @@ namespace os {
 			private:
 				void identity_cmd();
 				bool ata_init_success;
-				unsigned polling_limit;	
+				unsigned poll_lim;	
 		};
 	}
 }
